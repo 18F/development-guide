9106c82ca700f6272a63ca8a3dacb76b2c5ea2f9
Shawn Allen <shawn.allen@gsa.gov>
2016-02-11 14:05:26 -0800

    add poor person's accordion

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  // this is a poor person's accordion implementation
+  var toggleAccordion = function(e) {
+    var id = this.getAttribute('aria-controls');
+    var el = document.getElementById(id);
+    if (!el) {
+      console.warn('invalid aria-controls id:', id);
+      return false;
+    }
+    var expanded = this.getAttribute('aria-expanded') === 'false';
+    el.setAttribute('aria-hidden', !expanded);
+    this.setAttribute('aria-expanded', expanded);
+  };
+
+  var buttons = document.querySelectorAll('button[aria-controls]');
+  [].forEach.call(buttons, function(button) {
+    button.addEventListener('click', toggleAccordion);
+  });
+
+})();
