470aeb51949abac53b51ad45b1d58cf65efa603a
Shawn Allen <shawn.allen@gsa.gov>
2016-02-19 13:09:37 -0800

    add a poor person's scrollspy implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,65 @@
     button.addEventListener('click', toggleAccordion);
   });
 
+  /*
+   * a poor person's ScrollSpy implementation.
+   */
+  var scrollSpy = (function(links) {
+    // get a list of elements that the links target
+    var targets = links.map(function(link) {
+      var id = link.getAttribute('href').substr(1);
+      return document.getElementById(id);
+    });
+
+    // this is kind of like jQuery's ':visible' helper, but only pays attention
+    // to aria-hidden="true"
+    var isVisible = function(el) {
+      do {
+        if (el.getAttribute('aria-hidden') === 'true') {
+          return false;
+        }
+        el = el.parentNode;
+      } while (el && el !== nav);
+      return true;
+    };
+
+    // ratio of the screen height to treat as a buffer around the visible area
+    var buffer = .25;
+
+    return function(e) {
+      // set the upper and lower bounds based on the buffer
+      var top = window.innerHeight * buffer;
+      var bottom = window.innerHeight - top;
+
+      // first, determine which target is active
+      var active = targets.reduce(function(active, target, i) {
+        var rect = target.getBoundingClientRect();
+        // check that the content's bounding box is within our vertical bounds
+        if (rect.top <= bottom && rect.bottom >= top) {
+          // ensure that the link to it is visible (if it's not, then its
+          // parent section should be in bounds and its link should be
+          // visible)
+          if (isVisible(links[i])) {
+            return target;
+          }
+        }
+        return active;
+      }, null);
+
+      // then, set the aria-active attribute for both the link and target,
+      // based on which target is active
+      targets.forEach(function(target, i) {
+        var link = links[i];
+        target.setAttribute('aria-active', target === active);
+        link.setAttribute('aria-active', target === active);
+      });
+    };
+  })([].slice.call(nav.querySelectorAll('a[href^="#"]')));
+
+  // update the scroll spy on scroll, resize, and initial load
+  window.addEventListener('scroll', scrollSpy);
+  window.addEventListener('resize', scrollSpy);
+  scrollSpy();
 
   /*
    * automatically expand nav accordions whenever the location.hash references
