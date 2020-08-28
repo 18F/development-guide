3020dc6f617c5888d8f5fd64bf0786841a61e43a
Ryan Hofschneider <ryan.hofschneider@gsa.gov>
2020-08-28 14:46:39 -0700

    Add support for sub-organization specific themes
    By default website will just be affiliated with GSA/TTS. If you set the
    suborg key in override.yml to 18F, 18F branding will be used in the
    footer. Other sub-organizations can be added in similar fashion.
    
    Configuration is read in this order, last wins:
    - _config.yml
    - override.yml
    - YAML directly in _data
    - YAML in _data/suborgs/[18F, etc.]
    - YAML in _data/override

--- _plugins/override.rb
+++ _plugins/override.rb
@@ -6,23 +6,29 @@ module Jekyll
   # Loads 18F/uswds-jekyll YAML config files in OVERRIDE_DIR, overriding any keys found in _data.
 
   class OverrideGenerator < Generator
-    OVERRIDE_DIR = '_data/override/'
+    OVERRIDE_DIR = File.join("_data", "override")
+    SUBORGS_DIR = File.join("_data", "suborgs")
 
     safe true
 
     def generate(site)
       yamls = ['header', 'navigation', 'footer', 'theme', 'usa_anchor'] 
       yamls.each do |y|
-        self.merge(site, y)
+        if site.config['suborg']
+          self.merge(site, File.join(SUBORGS_DIR, site.config['suborg']), y)
+        end
+        self.merge(site, OVERRIDE_DIR, y)
       end
       # TODO Address favicons.yml special case
     end
 
-    def merge(site, key)
+    def merge(site, dir, key)
       begin
-        customizations = YAML.load_file(OVERRIDE_DIR + key + '.yml')
+        path = File.join(dir, key + '.yml')
+        customizations = YAML.load_file(path)
         if customizations
           site.data[key].merge!(customizations)
+          puts "        Merged " + path
         end
       rescue
       end
